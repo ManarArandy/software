ae5076dad97612baf1d9c44c9cfdd6ad1189df4e
Msys <Msys@DESKTOP-I0RDQJB.home>
2020-02-28 14:20:13 +0200

    y

--- test/test.java
+++ test/test.java
@@ -2,7 +2,7 @@ package calctest;
 
 import static org.junit.Assert.*;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-
+import org.junit.jupiter.api.BeforeAll;
 
 import org.junit.Test;
 import org.junit.jupiter.api.AfterAll;
