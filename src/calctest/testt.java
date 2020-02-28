dfeccc3304e0f175253fd1d16236e9f7ce8e0cf8
Msys <Msys@DESKTOP-I0RDQJB.home>
2020-02-28 15:11:15 +0200

    hihihihi

--- src/calctest/testt.java
+++ src/calctest/testt.java
@@ -1,99 +1,90 @@
 package calctest;
 
-import static org.junit.Assert.assertTrue;
-
-//import  org.junit.Assert.*;
-import org.junit.jupiter.api.BeforeAll;
-
-
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
-//import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 
 import calccode.calculator;
 
 public class testt {
+	 private static calculator object;
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		object=new calculator();
+	}
 
-	   private static calculator object;
-	    
-			@BeforeAll
-			static void setUpBeforeClass() throws Exception {
-				object=new calculator();
-			}
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
 
-			@AfterAll
-			static void tearDownAfterClass() throws Exception {
-			}
+	@Before
+	public void setUp() throws Exception {
+	}
 
-			@BeforeEach
-			void setUp() throws Exception {
-			}
+	@After
+	public void tearDown() throws Exception {
+	}
 
-			@AfterEach
-			void tearDown() throws Exception {
-			}
-			
-			
-			@Test
-			void testAdd_TwoPositiveInt_positive() {
-				// Arrange
-				int a = 1;
-				int b = 2;
-				
-				// Act
-				int actual = object.add(a,b);
-				
-				// Assert
-				assertTrue(actual==3);
-				
-			}
-			
-			@Test
-			void testAdd_TwonegativeInt_negative() {
-				// Arrange
-				int a = -1;
-				int b = -2;
-				// Act
-				int actual=object.add(a,b);
-				// Assert
-				assertTrue(actual==-3);
-				
-			}
-			@Test
-			void testAdd_PositiveNegativeInt_negative() {
-				// Arrange
-				int a = 1;
-				int b = -2;
-				// Act
-				int actual=object.add(a,b);
-				// Assert
-				assertTrue(actual==-1);
-				
-			}
-			@Test
-			void testAdd_PositiveNegativeInt_positive() {
-				// Arrange
-				int a = -1;
-				int b = 2;
-				// Act
-				int actual=object.add(a,b);
-				// Assert
-				assertTrue(actual==1);
-				
-			}
-			@Test
-			void testAdd_TwoPositiveNegativeEqualInt_zero() {
-				// Arrange
-				int a = -1;
-				int b = 1;
-				// Act
-				int actual=object.add(a,b);
-				// Assert
-				assertTrue(actual==0);
-				
-			}
+	@Test
+	void testAdd_TwoPositiveInt_positive() {
+		// Arrange
+		int a = 1;
+		int b = 2;
+		
+		// Act
+		int actual = object.add(a,b);
+		
+		// Assert
+		assertTrue(actual==3);
+		
+	}
+	
+	@Test
+	void testAdd_TwonegativeInt_negative() {
+		// Arrange
+		int a = -1;
+		int b = -2;
+		// Act
+		int actual=object.add(a,b);
+		// Assert
+		assertTrue(actual==-3);
+		
+	}
+	@Test
+	void testAdd_PositiveNegativeInt_negative() {
+		// Arrange
+		int a = 1;
+		int b = -2;
+		// Act
+		int actual=object.add(a,b);
+		// Assert
+		assertTrue(actual==-1);
+		
+	}
+	@Test
+	void testAdd_PositiveNegativeInt_positive() {
+		// Arrange
+		int a = -1;
+		int b = 2;
+		// Act
+		int actual=object.add(a,b);
+		// Assert
+		assertTrue(actual==1);
+		
+	}
+	@Test
+	void testAdd_TwoPositiveNegativeEqualInt_zero() {
+		// Arrange
+		int a = -1;
+		int b = 1;
+		// Act
+		int actual=object.add(a,b);
+		// Assert
+		assertTrue(actual==0);
+		
+	}
 
-		}
-
+}
