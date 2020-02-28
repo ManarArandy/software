68b3c0bb5bdde3073fa4ff97f57962a9179305ff
Msys <Msys@DESKTOP-I0RDQJB.home>
2020-02-28 15:58:12 +0200

    qwqw

---
+++ src/calctest/testtTest.java
@@ -0,0 +1,90 @@
+package calctest;
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import calccode.calculator;
+
+public class testtTest {
+	 private static calculator object;
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		object=new calculator();
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	void testAdd_TwoPositiveInt_positive() {
+		// Arrange
+		int a = 1;
+		int b = 2;
+		
+		// Act
+		int actual = object.add(a,b);
+		
+		// Assert
+		assertTrue(actual==3);
+		
+	}
+	
+	@Test
+	void testAdd_TwonegativeInt_negative() {
+		// Arrange
+		int a = -1;
+		int b = -2;
+		// Act
+		int actual=object.add(a,b);
+		// Assert
+		assertTrue(actual==-3);
+		
+	}
+	@Test
+	void testAdd_PositiveNegativeInt_negative() {
+		// Arrange
+		int a = 1;
+		int b = -2;
+		// Act
+		int actual=object.add(a,b);
+		// Assert
+		assertTrue(actual==-1);
+		
+	}
+	@Test
+	void testAdd_PositiveNegativeInt_positive() {
+		// Arrange
+		int a = -1;
+		int b = 2;
+		// Act
+		int actual=object.add(a,b);
+		// Assert
+		assertTrue(actual==1);
+		
+	}
+	@Test
+	void testAdd_TwoPositiveNegativeEqualInt_zero() {
+		// Arrange
+		int a = -1;
+		int b = 1;
+		// Act
+		int actual=object.add(a,b);
+		// Assert
+		assertTrue(actual==0);
+		
+	}
+
+}
